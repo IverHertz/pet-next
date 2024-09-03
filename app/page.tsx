0fd7a78ed0e01eddf348bf81aeb9f07fc4007559
Jazee <jazee@jaze.top>
2024-09-03 17:35:47 +0800

    feat: login

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,52 @@
+import {
+    Card,
+    CardContent,
+} from "@/components/ui/card"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs"
+import LoginForm from "@/components/form/login";
+import RegisterForm from "@/components/form/register";
+import Image from "next/image";
+
 export default function Home() {
     return (
-        <main className="flex flex-col min-h-screen">
+        <main className="w-full min-h-screen lg:grid lg:grid-cols-2">
+            <Image className="object-cover h-full" src="/login-side.png" alt="login-side" width={1280} height={1280}/>
 
+            <div className="flex justify-center items-center">
+                <div className="space-y-4">
+                    <h1 className="font-bold text-2xl">
+                        宠物领养管理系统
+                    </h1>
+                    <Tabs defaultValue="login" className="w-[400px]">
+                        <TabsList className="grid w-full grid-cols-2">
+                            <TabsTrigger value="login">登录</TabsTrigger>
+                            <TabsTrigger value="register">注册</TabsTrigger>
+                        </TabsList>
+
+                        <TabsContent value="login">
+                            <Card>
+                                <CardContent className="pt-4">
+                                    <LoginForm/>
+                                </CardContent>
+                            </Card>
+                        </TabsContent>
+
+                        <TabsContent value="register">
+                            <Card>
+                                <CardContent className="pt-4">
+                                    <RegisterForm/>
+                                </CardContent>
+                            </Card>
+                        </TabsContent>
+                    </Tabs>
+                </div>
+
+            </div>
         </main>
-    );
+    )
 }
---
+++ components/form/register.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import {z} from "zod"
+
+import {Button} from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import {Input} from "@/components/ui/input"
+
+const formSchema = z.object({
+    email: z.string().email({
+        message: "Invalid email address.",
+    }),
+    password: z.string().min(8, {
+        message: "Password must be at least 8 characters.",
+    })
+})
+
+export default function RegisterForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>邮箱</FormLabel>
+                            <FormControl>
+                                <Input placeholder="请输入邮箱" {...field} />
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({field}) => (
+                        <FormItem>
+                            <FormLabel>密码</FormLabel>
+                            <FormControl>
+                                <Input placeholder="请输入密码" {...field} />
+                            </FormControl>
+                            <FormMessage/>
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">注册</Button>
+            </form>
+        </Form>
+    )
+}
